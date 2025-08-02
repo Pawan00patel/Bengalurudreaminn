989fd9a9f044b637dc9ee8dc4f144a84c73357a8
Pawan00patel <pp5654826@gmail.com>
2025-08-02 19:48:08 +0530

    fixed build fail

--- src/components/layout/BengaluruDreaminNavbar.tsx
+++ src/components/layout/BengaluruDreaminNavbar.tsx
@@ -1,3 +1,5 @@
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import '../../styles/BengaluruDreamin2025.css';
